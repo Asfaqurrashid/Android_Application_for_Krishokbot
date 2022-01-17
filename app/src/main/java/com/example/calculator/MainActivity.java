37e9c2b401a0ed9d52dd55e714a46052310a08f7
asfaqurrashid <asfaqurrashid@gmail.com>
2022-01-18 01:15:28 +0600

    file changes

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.calculator;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.speech.tts.TextToSpeech;
+import android.text.TextUtils;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.NetworkError;
@@ -29,34 +35,79 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
     EditText question;
-    Button btn;
-    EditText textView;
-    Button reset;
+    Button button;
+    TextView textView;
     String url = "https://krishokbot.herokuapp.com/chatbot";
-    ImageView iv_mic;
+    ImageView ic_mic;
     TextToSpeech tts;
-    String data;
+    String data = "আপনি কেমন আছেন";
+    String audio = "";
+    private  static final int REQ_CODE_SPEECH_INPUT = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         question = (EditText) findViewById(R.id.question);
-        btn = (Button) findViewById(R.id.btn);
-        textView = (EditText) findViewById(R.id.textView);
-        reset = (Button) findViewById(R.id.reset);
-        iv_mic = (ImageView) findViewById(R.id.iv_mic);
-       btn.setOnClickListener(new View.OnClickListener() {
+        button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView);
+        ic_mic = (ImageView) findViewById(R.id.ic_mic);
+
+        ic_mic.setTag("ic_mic_off");
+        ic_mic.setImageResource(R.drawable.ic_mic_off);
+
+        ic_mic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(tts != null){
+                    tts.stop();
+                }
+                String bg = String.valueOf(ic_mic.getTag());
+                if(bg.equals("ic_mic_off")) {
+                    ic_mic.setImageResource(R.drawable.ic_mic);
+                    Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                    intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                    intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,"bn-BD");
+                    intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "কথা বলতে চাপুন");
+                    try {
+                        startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+                        ic_mic.setImageResource(R.drawable.ic_mic_off);
+                    }
+                    catch (Exception e) {
+                        Toast.makeText(MainActivity.this, " " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else {
+                    ic_mic.setImageResource(R.drawable.ic_mic_off);
+                }
+
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View view) {
+               String decide = "";
+               if(!TextUtils.isEmpty(question.getText().toString()) && !audio.equals("")){
+                   decide = audio;
+               }
+               else if(!TextUtils.isEmpty(question.getText().toString()) && audio.equals("")){
+                   decide = question.getText().toString();
+               }
+               else {
+                   decide = audio;
+               }
+
                Map<String,String> params = new HashMap<String,String>();
-               params.put("question", question.getText().toString());
+               params.put("question", decide);
                JSONObject jsonBody = new JSONObject(params);
                JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.POST,url,jsonBody ,new Response.Listener<JSONObject>() {
                    @Override
@@ -64,6 +115,20 @@ public class MainActivity extends AppCompatActivity {
                        try {
                            data = response.getString("response");
                            textView.setText(data);
+                           textView.setMovementMethod(new ScrollingMovementMethod());
+                           tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+                               @Override
+                               public void onInit(int i) {
+                                   if(i == TextToSpeech.SUCCESS){
+                                       ic_mic.setImageResource(R.drawable.ic_mic);
+                                       tts.setLanguage(Locale.ENGLISH);
+                                       tts.setSpeechRate(1.0f);
+                                       tts.speak(data,TextToSpeech.QUEUE_ADD,null);
+                                       }
+
+                                   }
+
+                               });
 
                        } catch (JSONException e) {
                            e.printStackTrace();
@@ -99,37 +164,24 @@ public class MainActivity extends AppCompatActivity {
 
                RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
                queue.add(jsonRequest);
+               audio = "";
            }
 
 
        });
 
-       reset.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View view) {
-               question.setText("");
-               textView.setText("");
-               question.setSelection(0);
-           }
-       });
-
-       iv_mic.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View view) {
-               tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
-                   @Override
-                   public void onInit(int i) {
-                       if(i == TextToSpeech.SUCCESS){
-                           tts.setLanguage(Locale.ENGLISH);
-                           tts.setSpeechRate(1.0f);
-                           tts.speak(data,TextToSpeech.QUEUE_ADD,null);
-                       }
-                   }
-               });
-           }
-       });
-
-
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQ_CODE_SPEECH_INPUT) {
+            if (resultCode == RESULT_OK && data != null) {
+                ArrayList<String> result = data.getStringArrayListExtra(
+                        RecognizerIntent.EXTRA_RESULTS);
+                audio = Objects.requireNonNull(result).get(0);
+            }
+        }
     }
 
 }
